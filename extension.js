e6d241a118d2c02d7faac32ea16665799927e066
Jonny Lamb <jonnylamb@jonnylamb.com>
2014-10-27 23:13:29 +0100

    extension: populate menu with downloaded json file

--- extension.js
+++ extension.js
@@ -18,11 +18,19 @@
 
 const BoxPointer = imports.ui.boxpointer;
 const GLib = imports.gi.GLib;
+const Gdk = imports.gi.Gdk;
+const Gtk = imports.gi.Gtk;
 const Lang = imports.lang;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const Soup = imports.gi.Soup;
 const St = imports.gi.St;
+
+const DATA_URL = "http://sciopero.jonnylamb.com/future.json";
+const DAYS_WARNING = 1;
+const ICON_UPCOMING = "dialog-warning-symbolic";
+const ICON_ONGOING = "dialog-warning";
 
 let sciopero = null;
 
@@ -41,6 +49,8 @@ const Sciopero = new Lang.Class({
     _init: function() {
         this.parent(0.0, "Roma Sciopero");
 
+        this.session = new Soup.Session();
+
         this.icon = new St.Icon();
         this.icon.set_icon_size(16);
 
@@ -51,11 +61,20 @@ const Sciopero = new Lang.Class({
     },
 
     update: function() {
-        /* TODO */
-        this.icon.set_icon_name("dialog-warning-symbolic");
-        this.actor.visible = alwaysShow || true;
+        let uri = new Soup.URI(DATA_URL);
+        let message = new Soup.Message({method: "GET", uri: uri});
 
-        this.addElement("Sciopero Atac e trasporto pubblico: stop 24 ore 1° ottobre 2014", function() {});
+        this.session.queue_message(message, Lang.bind(this, this.response));
+    },
+
+    response: function(session, message) {
+        if (!message.response_body.data) {
+            debug("no data in the response");
+            return;
+        }
+
+        let j = JSON.parse(message.response_body.data);
+        this.parse(j);
     },
 
     addElement: function(title, func) {
@@ -68,6 +87,65 @@ const Sciopero = new Lang.Class({
         this.menu.addMenuItem(menuItem);
 
         debug("added element '" + title + "'");
+    },
+
+    parse: function(data) {
+        let ongoing = false;
+        let upcoming = false;
+
+        debug("starting parsing");
+
+        let now = new Date();
+
+        for (let i = 0; i < data.length; i++) {
+            let item = data[i];
+
+            let from = new Date(item.from);
+            let ends = new Date(item.ends);
+
+            if (now <= ends && now >= from) {
+                ongoing = true;
+                debug("found one ongoing");
+            } else if (now > ends) {
+                debug("found one in the future");
+                continue;
+            } else {
+                // this is pretty ugly
+                from.setHours(0);
+                from.setMinutes(0);
+                from.setSeconds(0);
+                from.setMilliseconds(0);
+
+                now.setHours(0);
+                now.setMinutes(0);
+                now.setSeconds(0);
+                now.setMilliseconds(0);
+
+                let difference = Math.round((from - now) / 1000 / 60 / 60 / 24);
+
+                debug("found one in " + difference + " day(s)");
+
+                if (difference <= DAYS_WARNING) {
+                    upcoming = true;
+                }
+            }
+
+            if (upcoming || ongoing) {
+                this.addElement(item.title, function() {
+                    Gtk.show_uri(null, item.link, Gdk.CURRENT_TIME);
+                });
+            }
+        }
+
+        if (ongoing) {
+            this.icon.set_icon_name(ICON_ONGOING);
+        } else if (upcoming) {
+            this.icon.set_icon_name(ICON_UPCOMING);
+        }
+
+        this.actor.visible = alwaysShow || ongoing || upcoming;
+
+        debug("finished parsing");
     }
 });
 
